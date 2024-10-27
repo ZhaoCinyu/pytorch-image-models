e910180375410fbf8c9dfbb312c6108780438c9d
Mufan Qiu <mufan@cs.unc.edu>
2024-10-27 12:32:24 +0000

    add ckpt

---
+++ running_scripts/mobile_vit_eval.sh
@@ -0,0 +1,17 @@
+# dense model
+# MODEL=mobilevitv2_100 # not pretrained
+# MODEL=mobilevitv2_100.cvnets_in1k # pretrained
+
+# not pretrained
+
+HOME=/data/mufan/jie/pytorch-image-models
+
+MODEL=mobilevitv2_100_moe
+
+# pretrained, TODO: map the dense weights and partition into MoE
+# MODEL=mobilevitv2_100_moe.cvnets_in1k
+
+DATASET='hfids/uoft-cs/cifar10'
+
+CUDA_VISIBLE_DEVICES=0 python mobilevit_inference.py --num_epochs 1
+    # --pretrained \ # for pretrained model
